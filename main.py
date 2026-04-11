6355c0e0c7fc0fa9551de9fc86cb1ff4e035bfb5
mmatrix05 <mmatrix05@gmail.com>
2026-04-12 00:54:43 +0200

    korigiran prikaz

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ import streamlit as st
 import math
 from plot_presjek import nacrtaj_presjek
 
-st.title("Proračun uzgona - okno")
+st.set_page_config(layout="wide")
+st.markdown("""
+<style>
+.block-container {
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+}
+</style>
+""", unsafe_allow_html=True)
+
+col_space, col_title = st.columns([1.2, 1.8])
+
+with col_title:
+    st.title("Kontrola uzgona okna")
+
+col1, col2 = st.columns([1, 1])
+
+#with col1:
 
 st.sidebar.header("Ulazni podaci")
 
@@ -21,6 +38,11 @@ b = st.sidebar.number_input("Debljina betonskog prstena (m)", value=0.2)
 gamma_beton = st.sidebar.number_input("Spec. težina betona (kN/m3)", value=25.0)
 gamma_tla = st.sidebar.number_input("Spec. težina tla (kN/m3)", value=18.0)
 gamma_vode = st.sidebar.number_input("Spec. težina vode (kN/m3)", value=10.0)
+
+# ------------------------
+# FAKTOR SIGURNOSTI
+# ------------------------
+FS_min = st.sidebar.number_input("Traženi faktor sigurnosti", value=1.10)
 
 # ------------------------
 # KONTROLA KOTA
@@ -60,11 +82,6 @@ H_uzgon = max(0, nivo_vode - (niveleta - t))
 U = gamma_vode * A_temelj * H_uzgon
 
 # ------------------------
-# FAKTOR SIGURNOSTI
-# ------------------------
-FS_min = st.sidebar.number_input("Traženi faktor sigurnosti", value=1.10)
-
-# ------------------------
 # ITERACIJA VISINE PRSTENA
 # ------------------------
 
@@ -74,7 +91,9 @@ korak = 0.05  # 5 cm
 max_h = H_ukupno  # ne može biti viši od okna
 
 FS = 0
-
+h_ok = 0
+FS_ok = 0
+G_ok = 0
 while FS <= FS_min:
 
     # TEŽINE
@@ -115,86 +134,97 @@ while FS <= FS_min:
     if h_prsten >= H_ukupno - 0.05:
         break
 
+    h_ok = h_prsten
+    FS_ok = FS
+    G_ok = G_total
+
     h_prsten += korak
 
+h_prsten = h_ok
+FS = FS_ok
+G_total = G_ok
+
 # KOREKCIJS MINIMSLNOG PRSTENA NAKON +korak
-if h_prsten == 0.05:
-    h_prsten = 0
+#if h_prsten == 0.05:
+#    h_prsten = 0
 
 # ------------------------
 # SUMA
 # ------------------------
 G_total = G1 + G2 + G3 + G3A + G4 + G4A
 
-# ------------------------
-# REZULTATI
-# ------------------------
-st.header("Rezultati")
+with col1:
+    # ------------------------
+    # REZULTATI
+    # ------------------------
+    st.markdown("<h3 style='margin-top:0;'>Rezultati</h3>", unsafe_allow_html=True)
 
-st.write(f"Dubina okna = {H_ukupno:.2f} m")
-st.write(f"Visina podzemne vode = {H_uronjeno:.2f} m")
+    st.write(f"Dubina okna = {H_ukupno:.2f} m")
+    st.write(f"Visina podzemne vode = {H_uronjeno:.2f} m")
 
-st.write(f"Uzgon U = {U:.2f} kN")
-st.write(f"Ukupna težina ΣG = {G_total:.2f} kN")
+    st.write(f"Uzgon U = {U:.2f} kN")
+    st.write(f"Ukupna težina ΣG = {G_total:.2f} kN")
 
-if U <= 0:
-    FS = float("inf")
-    h_prsten = 0
+    if U <= 0:
+        FS = float("inf")
+        h_prsten = 0
 
-    st.write("Sigurnost protiv uzgona = ∞")
-    st.success("Nema uzgona – prsten nije potreban")
-else:
-    FS = G_total / U
-    st.write(f"Sigurnost protiv uzgona = {FS:.2f}")
+        st.write("Sigurnost protiv uzgona = ∞")
+        st.success("Nema uzgona – prsten nije potreban")
+    else:
+        FS = G_total / U
+        st.write(f"Sigurnost protiv uzgona = {FS:.2f}")
 
-st.write(f"Potrebna visina prstena = {h_prsten:.2f} m")
+    st.write(f"Potrebna visina prstena = {h_prsten:.2f} m")
 
-if FS >= FS_min:
-    st.success("OK – zadovoljava")
-else:
-    st.error("NE zadovoljava")
+    if FS >= FS_min:
+        st.success("OK – zadovoljava")
+    else:
+        st.error("NE zadovoljava")
 
-# ------------------------
-# KONTROLNI ISPIS
-# ------------------------
-st.subheader("Kontrola proračuna")
+    # ------------------------
+    # KONTROLNI ISPIS
+    # ------------------------
+    st.subheader("Kontrola proračuna")
 
-st.write(f"G1 - Temeljna ploča = {G1:.2f} kN")
-st.write(f"G2 - Betonski prsten = {G2:.2f} kN")
-st.write(f"G3 - Uronjeno tlo (unutar prstena) = {G3:.2f} kN")
-st.write(f"G3A - Uronjeno tlo (proširenje) = {G3A:.2f} kN")
-st.write(f"G4 - Suho tlo (unutar prstena) = {G4:.2f} kN")
-st.write(f"G4A - Suho tlo (proširenje) = {G4A:.2f} kN")
+    st.write(f"G1 - Temeljna ploča = {G1:.2f} kN")
+    st.write(f"G2 - Betonski prsten = {G2:.2f} kN")
+    st.write(f"G3 - Uronjeno tlo (unutar prstena) = {G3:.2f} kN")
+    st.write(f"G3A - Uronjeno tlo (proširenje) = {G3A:.2f} kN")
+    st.write(f"G4 - Suho tlo (unutar prstena) = {G4:.2f} kN")
+    st.write(f"G4A - Suho tlo (proširenje) = {G4A:.2f} kN")
 
-# ------------------------
-# DEBUG
-# ------------------------
-st.subheader("Debug")
+    # ------------------------
+    # DEBUG
+    # ------------------------
+    st.subheader("Debug")
 
-st.write(f"A_temelj = {A_temelj:.3f} m2")
-st.write(f"A_prstena = {A_prsten:.3f} m2")
-st.write(f"A_okna = {A_okno:.3f} m2")
-st.write(f"A_prosirenje = {A_prosirenje:.3f} m2")
-st.write(f"H_uronjeno = {H_uronjeno:.3f} m")
-st.write(f"H_suho = {H_suho:.3f} m")
-st.write(f"H_ukupno = {H_ukupno:.3f} m")
-st.write(f"H_uronjeno iznad = {H_uronjeno_iznad:.3f} m")
-st.write(f"H_prstena = {h_prsten:.3f} m")
+    st.write(f"A_temelj = {A_temelj:.3f} m2")
+    st.write(f"A_prstena = {A_prsten:.3f} m2")
+    st.write(f"A_okna = {A_okno:.3f} m2")
+    st.write(f"A_prosirenje = {A_prosirenje:.3f} m2")
+    st.write(f"H_uronjeno = {H_uronjeno:.3f} m")
+    st.write(f"H_suho = {H_suho:.3f} m")
+    st.write(f"H_ukupno = {H_ukupno:.3f} m")
+    st.write(f"H_uronjeno iznad = {H_uronjeno_iznad:.3f} m")
+    st.write(f"H_prstena = {h_prsten:.3f} m")
+    st.write(f"H_prstena = {h_ok:.3f} m")
 
-st.subheader("Grafički prikaz")
+with col2:
+    #st.header("Grafički prikaz")
+    st.markdown("<h3 style='margin-top:0;'>Grafički prikaz</h3>", unsafe_allow_html=True)
 
-fig = nacrtaj_presjek(
-    teren,
-    niveleta,
-    nivo_vode,
-    D,
-    a,
-    t,
-    b,
-    h_prsten,
-    H_uzgon,
-    H_uronjeno,
-    H_suho
-)
-
-st.pyplot(fig)
+    fig = nacrtaj_presjek(
+        teren,
+        niveleta,
+        nivo_vode,
+        D,
+        a,
+        t,
+        b,
+        h_prsten,
+        H_uzgon,
+        H_uronjeno,
+        H_suho
+    )
+    st.pyplot(fig, use_container_width=True)
--- plot_presjek.py
+++ plot_presjek.py
@@ -16,7 +16,14 @@ def nacrtaj_presjek(
     H_uronjeno,
     H_suho
 ):
-    fig, ax = plt.subplots(figsize=(6, 8))
+    y0 = niveleta  # referenca
+
+    y_teren = teren - y0
+    y_niveleta = 0
+    y_dno = -t
+    y_voda = nivo_vode - y0
+
+    fig, ax = plt.subplots(figsize=(6, 6))
 
     # KOTE
     y_teren = teren
@@ -51,7 +58,7 @@ def nacrtaj_presjek(
         H_ukupno,
         facecolor='white',
         edgecolor='black',
-        zorder=5
+        zorder=8
     ))
 
     # ------------------------
@@ -121,15 +128,27 @@ def nacrtaj_presjek(
 
     # OZNAKE
     ax.text(0, y_teren, "Teren", ha='center', va='bottom')
-    ax.text(0, y_niveleta, "Niveleta", ha='center', va='top')
+    ax.text(0, y_niveleta, "Niveleta", ha='center', va='bottom', zorder=10)
     ax.text(0, y_voda, "Voda", ha='center', va='bottom', zorder=10)
+
+    # h_prsten
+    ax.annotate(
+        '',
+        xy=(r_vanjski + 0.3, niveleta),
+        xytext=(r_vanjski + 0.3, niveleta + h_prsten),
+        arrowprops=dict(arrowstyle='<->')
+    )
+    ax.text(r_vanjski + 0.35, niveleta + h_prsten / 2, f"h = {h_prsten:.2f} m", rotation=90, va='center')
+
+    ax.text(0.2, y_teren - H_ukupno * 0.4, "G", va='top')
 
     # POSTAVKE
     ax.set_xlim(-half_a - 0.5, half_a + 0.5)
     ax.set_ylim(y_dno - 1, y_teren + 1)
 
-    ax.set_aspect('equal')
+    ax.set_aspect('auto')
     ax.axis('off')
-    ax.set_title("Presjek okna")
-
+    #ax.set_title("Presjek okna")
+    fig.tight_layout()
+    ax.set_ylim(y_dno - 0.2, y_teren + 0.1)
     return fig
